31bf4dc2a13ea88376bc66c0bb0531de790fe437
JMB2K <jonathanmb2000@outlook.com>
2017-10-19 09:39:01 -0500

    cleaned up code and added bookmarks to combined PDF

---
+++ spectool.py
@@ -0,0 +1,72 @@
+#!python3
+
+import os.path, os, PyPDF2
+from collections import OrderedDict as OD
+
+location = os.getcwd()
+directory = os.listdir(location)
+pdfWriter = PyPDF2.PdfFileWriter()
+pages=0
+
+
+def rename_to_spaces(location):
+    for file in directory:
+        if ' ' not in file[:6] and file[0].isdigit():
+            # print(new_filename)
+            a = file[:2]
+            b = file[2:4]
+            c = file[4:6]
+            d = file[7:]
+            names = [a, b, c, d]
+            new_filename = ' '.join(names)
+            os.rename(os.path.join(location, file), os.path.join(location, new_filename))
+
+
+def rename_no_spaces(location):
+    for file in directory:
+        if file[0].isdigit() and file[2] == ' ':
+            a = file[:2]
+            b = file[3:5]
+            c = file[6:8]
+            d = file[8:]
+            names = [a, b, c, d]
+            new_filename = ''.join(names)
+            os.rename(os.path.join(location, file), os.path.join(location, new_filename))
+
+
+def build_specs(location):
+    global pages
+    rename_to_spaces(location)
+    pdfWriter = PyPDF2.PdfFileWriter()
+    blank='no'
+    bookmarks=OD()
+
+    def write_to_file(sect):
+        global pages
+        for pageNum in range(pdfReader.numPages):
+            pages+=1
+            pageObj = pdfReader.getPage(pageNum)
+            pdfWriter.addPage(pageObj)
+
+
+    for file in directory:
+        pdfFile = open(file, 'rb')
+        pdfReader = PyPDF2.PdfFileReader(pdfFile, strict=False)
+        bookmarks[file] = pages
+
+        write_to_file(file)
+
+        if pdfReader.numPages % 2 != 0:
+            if blank == 'yes':
+                pdfWriter.addBlankPage()
+                pages+=1
+            else:
+                blank = 'yes'
+
+    for books in bookmarks:
+        pdfWriter.addBookmark(books, bookmarks[books])
+
+    OutputFile = open('CombinedSpecs.pdf', 'wb')
+    pdfWriter.write(OutputFile)
+    OutputFile.close()
+    pdfFile.close()
