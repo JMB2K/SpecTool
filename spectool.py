1177b46fcf8314fef6a870f692e9992440b046df
Jonathan Burk <jb@Jonathans-iMac.local>
2018-03-04 05:53:07 -0600

    changed gui and removed nonsense

--- spectool.py
+++ spectool.py
@@ -1,32 +1,28 @@
+import PyPDF2
+import os
+
 from appJar import gui
 from collections import OrderedDict as OD
-import os
-import PyPDF2
-
 
 def execute(btnName):
     global location
-    task = app.getRadioButton('task')
-    options = app.getRadioButton('options')
     location = app.getEntry('location')
+    opts = app.option('Options')
 
     if location == '':
-        app.errorBox('Empty Location', 'Choose the directory with the files in it.')
+        app.popUp('Empty Location', 'Choose the directory with the files in it.', kind='warning')
     else:
         directory = os.listdir(location)
-        if task == 'Build Specs':
+        if opts == 'Build Specs':
             build_specs(directory)
-        elif task == 'Rename Files':
-            if options == 'Add Spaces':
-                rename_to_spaces(directory)
-            elif options == 'Remove Spaces':
-                rename_no_spaces(directory)
-
+        elif opts == 'Add Spaces':
+            rename_to_spaces(directory)
+        elif options == 'Remove Spaces':
+            rename_no_spaces(directory)
 
 def rename_to_spaces(directory):
     for file in directory:
         if ' ' not in file[:6] and file[0].isdigit():
-            # print(new_filename)
             a = file[:2]
             b = file[2:4]
             c = file[4:6]
@@ -35,18 +31,13 @@ def rename_to_spaces(directory):
             new_filename = ' '.join(names)
             os.rename(os.path.join(location, file), os.path.join(location, new_filename))
 
-
 def rename_no_spaces(directory):
     for file in directory:
         if file[0].isdigit() and file[2] == ' ':
-            a = file[:2]
-            b = file[3:5]
-            c = file[6:8]
-            d = file[8:]
-            names = [a, b, c, d]
-            new_filename = ''.join(names)
+            first = file[:8].replace(' ', '')
+            last = file[8:]
+            new_filename = first + last
             os.rename(os.path.join(location, file), os.path.join(location, new_filename))
-
 
 def build_specs(directory):
     global pages, pdfFile
@@ -55,23 +46,19 @@ def build_specs(directory):
     bookmarks = OD()
     os.chdir(location)
 
-    def write_to_file():
-        global pages
-        for pageNum in range(pdfReader.numPages):
-            pages += 1
-            pageObj = pdfReader.getPage(pageNum)
-            pdfWriter.addPage(pageObj)
-
     for file in directory:
         try:
             pdfFile = open(file, 'rb')
             pdfReader = PyPDF2.PdfFileReader(pdfFile, strict=False)
             bookmarks[file] = pages
         except Exception:
-            app.errorBox('Stupid File', '{} is stupid.  \nResave the PDF and try again'.format(file))
+            app.popUp('Stupid File', '{} is stupid.  \nResave the PDF and try again'.format(file), kind='error')
             return
 
-        write_to_file()
+        for pageNum in range(pdfReader.numPages):
+            pages += 1
+            pageObj = pdfReader.getPage(pageNum)
+            pdfWriter.addPage(pageObj)
 
         if pdfReader.numPages % 2 != 0:
             pdfWriter.addBlankPage()
@@ -88,22 +75,12 @@ def build_specs(directory):
 
 if __name__ == '__main__':
 
-
     app =  gui("SpecTool")
-    app.setBg("dark gray")
-    app.setFont(15, font="Google Sans Medium")
+    app.font = 16
     app.setSticky('ew')
     app.setTitle('SpecTool')
 
-    app.startLabelFrame('Task')
-    app.addRadioButton('task', 'Build Specs')
-    app.addRadioButton('task', 'Rename Files')
-    app.stopLabelFrame()
-
-    app.startLabelFrame('Renaming Options')
-    app.addRadioButton('options', 'Add Spaces')
-    app.addRadioButton('options', 'Remove Spaces')
-    app.stopLabelFrame()
+    app.option('Options', ['Build Specs', '- Rename Files -', 'Add Spaces', 'Remove Spaces'])
 
     app.addDirectoryEntry('location')
     app.addButton('Start', execute)
